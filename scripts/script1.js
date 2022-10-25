d05cd8ced30e48e8b6826ce4f5b394ce425707e0
RozekRozek <allroszkowski@gmail.com>
2022-10-25 08:58:06 +0200

    Finalizing

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,3 +2,5 @@ function ChangeColor(){
     let color = document.getElementById('ColorInput').value;
     document.getElementById("ColorPlate").style.backgroundColor = color;
 }
+
+
---
+++ scripts/script2.js
@@ -0,0 +1,11 @@
+
+function Add_Sculpture() {
+    var div = document.createElement('div');
+    div.setAttribute("id","SculptureStand");
+    var img = document.createElement('img');
+    img.src = '../misc/Dawid.jpg';
+    document.getElementById('Holder').appendChild(div);
+    document.getElementById('SculptureStand').appendChild(img);
+    var button = document.getElementById('SculptureButton');
+    button.disabled = "disabled"
+} 
